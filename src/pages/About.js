d6cc991051a5d75496007412d7da62ae06ac5a55
anjdurel@gmail.com <alenadurel@Alenas-MacBook-Pro.local>
2022-07-20 19:23:20 -0400

    Week 6 complete

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,31 +1,35 @@
-import React, { Component } from "react";
-import "./About.css";
-import profile_pic from "../assets/profile_pic.jpg";
-
+import React, { Component } from 'react';
+import "./About.css"
+import profile_pic_Alena from "../assets/profile_pic_Alena.jpg";
+  
 export default class About extends Component {
   render() {
     return (
       <div>
-        {/* <p>Design your About me page </p> */}
-        <div class="split left">
-          <div className="centered">
-            <img
-              className="profile_image"
-              src={profile_pic}
-              alt="Profile Pic"
-            ></img>
-          </div>
-        </div>
-        <div className="split right">
-          <div className="centered">
-            <div className="name_title">Your Name</div>
-            <div className="brief_description">
-              Tell us about yourself in a few sentences. Tell us your interests
-              and say a fun fact about yourself.
+        <div>
+          <div class="split left">
+            <div className="centered">
+              <img 
+                className="profile_image"
+                src = {profile_pic_Alena}
+                alt="Profile Pic"
+                ></img>
             </div>
           </div>
+        <div className="split right">
+        <div className="centered">
+        <div className="name_title">Alena Durel</div>
+        <div className="brief_description">
+          Hello! I am a rising junior at Charleston Southern University studying
+          Computer Science and Cybersecurity. I'm interested in learning more
+          about full stack development, software development, and digital
+          forensics.
         </div>
       </div>
-    );
+    </div>
+  </div>
+
+  </div>
+    )
   }
 }
